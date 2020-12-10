5ad4a930ca9b41b3798116b813475b26056b01db
introkun <introkun@gmail.com>
2020-12-10 23:04:17 +0300

    lesson 12 done

--- src/App.js
+++ src/App.js
@@ -12,8 +12,12 @@ class App extends Component {
       <div style={divStyle}>
         <h1>Hello world!</h1>
 
-        <Car name={'Ford'} year={2018} />
-        <Car name="Audi" year={2016} />
+        <Car name={'Ford'} year={2018}>
+          <p style={{color: 'blue'}}>COLOR</p>
+        </Car>
+        <Car name="Audi" year={2016}>
+          <p style={{color: 'red'}}>COLOR</p>
+        </Car>
         <Car name={'Mazda'} year={2010} />
       </div>
     );
