cb7ae34ad33ce3a604ea71b13eee2e381db4c9c4
introkun <introkun@gmail.com>
2020-12-10 22:17:11 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Car from './Car/Car'
+
+class App extends Component {
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    return (
+      <div style={divStyle}>
+        <h1>Hello world!</h1>
+
+        <Car />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Car/Car.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+// function car() {
+//   return (
+//     <div>This is car component</div>
+//   )
+// }
+
+// const car = () => {
+//   return (
+//     <div>This is car component</div>
+//   )
+// }
+
+// const car = () => (
+//   <div>
+//     This is car component
+//     <strong>test</strong>
+//   </div>
+// )
+
+export default () => (
+  <div>
+    This is car component
+    <strong>test</strong>
+  </div>
+)
