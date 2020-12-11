59e070ee0f08150f1d564e872a3395425f5fd9b9
introkun <introkun@gmail.com>
2020-12-11 22:55:26 +0300

    lesson 18 done

--- src/App.js
+++ src/App.js
@@ -37,21 +37,15 @@ class App extends Component {
         <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}
         >Change title</button>
 
-        <Car
-          name={cars[0].name}
-          year={cars[0].year}
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
-        />
-        <Car
-          name={cars[1].name}
-          year={cars[1].year}
-          onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
-        />
-        <Car
-          name={cars[2].name}
-          year={cars[2].year}
-          onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
-        />
+        { this.state.cars.map((car, index) => {
+          return (
+            <Car
+              key={index}
+              name={car.name}
+              year={car.year}
+              onChangeTitle={() => this.changeTitleHandler(car.name)} />
+          )
+        }) }
       </div>
     );
   }
