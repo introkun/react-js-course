6ef7cd0574a0e367c9b86bff87fd1e31e4098734
introkun <introkun@gmail.com>
2020-12-12 12:59:26 +0300

    lesson 22 done

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,13 +1,8 @@
 import React from 'react';
+import './Car.css'
 
 const car = props => (
-  <div style={{
-    border: "1px solid #ccc",
-    marginBottom: '10px',
-    padding: "10px",
-    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-    borderRadius: '5px'
-  }}>
+  <div className="Car">
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
     <input type="text" onChange={props.onNameChange} value={props.name} />
