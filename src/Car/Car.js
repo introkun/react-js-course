00ad2e64d3686880d29691767af052c0a54cb99c
introkun <introkun@gmail.com>
2020-12-12 13:10:21 +0300

    lesson 23 done

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Car.css'
 
-const car = props => (
+const car = props => {
+  const inputClasses = ['input']
+
+  if (props.name)
+    inputClasses.push('green')
+  else
+    inputClasses.push('red')
+
+  if (props.name.length > 4)
+    inputClasses.push('bold')
+
+  return (
   <div className="Car">
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
-    <input type="text" onChange={props.onNameChange} value={props.name} />
+    <input
+      type="text"
+      onChange={props.onNameChange}
+      value={props.name}
+      className={inputClasses.join(' ')} />
     <button onClick={props.onDelete}>Delete</button>
   </div>
-)
+  )
+}
 
 export default car
