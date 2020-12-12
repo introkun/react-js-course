d34ecc818d0ff1f6393a14d169646bfc9875aa6b
introkun <introkun@gmail.com>
2020-12-12 18:55:54 +0300

    lesson 24 done

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from 'react'
+import Radium from 'radium'
 import './Car.css'
 
-const car = props => {
+const Car = props => {
   const inputClasses = ['input']
 
   if (props.name)
@@ -12,8 +13,18 @@ const car = props => {
   if (props.name.length > 4)
     inputClasses.push('bold')
 
+  const style = {
+    border: '1px solid #ccc',
+    boxShadow: '0 4px 5px 0 rgba(0, 0, 0, 0.14)',
+    ':hover': {
+      border: '1px solid #aaa',
+      boxShadow: '0 4px 15px 0 rgba(0, 0, 0, 0.25)',
+      cursor: 'pointer'
+    }
+  }
+
   return (
-  <div className="Car">
+  <div className="Car" style={style}>
     <h3>Car name: {props.name}</h3>
     <p>Year: <strong>{props.year}</strong></p>
     <input
@@ -26,4 +37,4 @@ const car = props => {
   )
 }
 
-export default car
+export default Radium(Car)
