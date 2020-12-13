90e2ff549e3f30f3f4df5e3d9f8c3dbd8a51321a
introkun <introkun@gmail.com>
2020-12-13 13:02:06 +0300

    lesson 34 done

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
     this.state = {
       cars: [
         {name: 'Ford', year: '2018'},
-        // {name: 'Audi', year: '2016'},
-        // {name: 'Mazda', year: '2010'}
+        {name: 'Audi', year: '2016'},
+        {name: 'Mazda', year: '2010'}
       ],
       pageTitle: 'React components',
       showCars: false
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -24,6 +24,10 @@ class Car extends React.Component {
     console.log('Car componentDidUpdate')
   }
 
+  componentWillUnmount() {
+    console.log('Car componentWillUnmount')
+  }
+
   render() {
     console.log('Car render')
     const inputClasses = ['input']
