2aa1b0e35c1cb33294be79fa16f83d29ddaaf118
introkun <introkun@gmail.com>
2020-12-13 12:21:59 +0300

    lesson 29 done

--- src/App.js
+++ src/App.js
@@ -54,7 +54,8 @@ class App extends Component {
 
     return (
       <div style={divStyle}>
-        <h1>{this.state.pageTitle}</h1>
+        {/* <h1>{this.state.pageTitle}</h1> */}
+        <h1>{this.props.title}</h1>
 
         <button onClick={this.toggleCarsHandler}>Toggle cars</button>
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App title="I'm from props!" />, document.getElementById('root'));
 registerServiceWorker();
